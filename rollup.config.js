08978cd520326e8efe83602de7c2429d1618fb65
Stefano J. Attardi <stefano@storehouse.co>
2016-03-24 20:11:10 -0700

    Clean up Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,10 @@
 import babel from 'rollup-plugin-babel';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   entry: 'src/index.js',
-  moduleName: 'Stylistic',
-  globals: {
-    react: 'React',
-  },
   plugins: [
     babel({
-      presets: ['es2015-rollup', 'react', 'stage-0'],
-    }),
-    nodeResolve({
-      jsnext: true,
-      main: true,
-      skip: ['react'],
-    }),
-    commonjs({
-      include: 'node_modules/**',
+      presets: ['es2015-rollup'],
     }),
   ],
 };
